400d546e566d4e64f69b33b7d3de164b7a733729
Radek Jancik <radek.jancik@usys.cz>
2025-12-21 21:26:37 +0100

    25.12.21.1 feat: Added net10.0 to TargetFrameworks

--- SunamoPercentCalculator/_sunamo/SunamoExceptions/ThrowEx.cs
+++ SunamoPercentCalculator/_sunamo/SunamoExceptions/ThrowEx.cs
@@ -1,6 +1,7 @@
+namespace SunamoPercentCalculator._sunamo.SunamoExceptions;
+
 // EN: Variable names have been checked and replaced with self-descriptive names
 // CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-namespace SunamoPercentCalculator._sunamo.SunamoExceptions;
 internal partial class ThrowEx
 {
     internal static bool DivideByZero() { return ThrowIsNotNull(Exceptions.DivideByZero(FullNameOfExecutedCode())); }
